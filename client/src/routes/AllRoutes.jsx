dfe8f383cd7506cc73efa0dc16a1dc547270babf
Shivam Kumar <shivam.ownwork@gmail.com>
2026-04-28 23:52:55 +0530

    google client and server setup done.

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
+import Login from '../pages/Login'
+import App from '../App'
 
 const AllRoutes = () => {
 
-    const routes = createBrowserRouter([
-        {
-            path:"/",
-            element:""
-        }
-    ])
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ])
 
-  return (
-    <div>AllRoutes</div>
-  )
+  return routes
 }
 
 export default AllRoutes
